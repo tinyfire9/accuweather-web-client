c4140501c4989c3b4cf6a86375e43fe0d091a432
slimfire <personalprojectyohannes@gmail.com>
2020-03-04 19:07:55 -0500

    [TASK] Setup react app

---
+++ src/constants/http-response.ts
@@ -0,0 +1,10 @@
+
+import { HTTPErrorResponse } from "../interfaces";
+
+export let httpErrorResponse :HTTPErrorResponse = {
+    "400": "Request had bad syntax or the parameters supplied were invalid.",
+    "401": "Unauthorized. API authorization failed.", 
+    "503": "Unauthorized. You do not have permission to access this endpoint.", 
+    "404": "Server has not found a route matching the given URI.", 
+    "500": "Server encountered an unexpected condition which prevented it from fulfilling the request."
+};
