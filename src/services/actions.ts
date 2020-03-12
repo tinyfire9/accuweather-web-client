0305b657e6fb6baf164f8ae0ea25810514f840de
slimfire <personalprojectyohannes@gmail.com>
2020-03-11 20:31:14 -0700

    [TASK] Add action creators

---
+++ src/services/actions.ts
@@ -0,0 +1,17 @@
+import { FEATURE, SERVICE } from '../constants';
+import { ACTION_TYPE, Action } from './';
+
+export let fetchDataAction = (feature: FEATURE, service: SERVICE, route?: string): Action => ({
+    type: ACTION_TYPE.FETCH_DATA,
+    feature, service, route,
+});
+
+export let fetchDataErrorAction = (feature: FEATURE, service: SERVICE, status: number, statusText?: string): Action => ({
+    type: ACTION_TYPE.FETCH_DATA_ERROR,
+    feature, service, status, statusText,
+});
+
+export let fetchDataSuccessAction = (feature: FEATURE, service: SERVICE, status: number, body?: any, statusText?: string): Action => ({
+    type: ACTION_TYPE.FETCH_DATA_SUCCESS,
+    feature, service, status, statusText, body,
+});
