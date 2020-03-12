e14dc50140a40767adff3cbb7ba309bdf8cfd5bc
slimfire <personalprojectyohannes@gmail.com>
2020-03-11 20:37:25 -0700

    [TASK] Store API versions, features and services as constants. Add makeURL

--- src/constants.ts
+++ src/constants.ts
@@ -9,9 +9,22 @@ export let httpErrorResponse :HTTPErrorResponse = {
     '500': 'Server encountered an unexpected condition which prevented it from fulfilling the request.'
 };
 
-export enum ENTITIES {
-    locations = 'locations',
+export enum FEATURE {
+    country = 'countries',
+    region = 'regions'
+}
+
+export enum SERVICE {
+    locations = 'locations'
+}
+
+export enum API_VERSION {
+    v1 = 'v1'
 }
 
 export let API_KEY: string  = '';
 export let BASE_URL: string = 'http://dataservice.accuweather.com'
+
+export let makeURL = (service: SERVICE, route?: string) => {
+    return `${BASE_URL}/${service}/${API_VERSION.v1}${route}?apikey=${API_KEY}`;
+}
