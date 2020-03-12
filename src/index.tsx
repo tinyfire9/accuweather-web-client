8b7ca3767018eb09e3e98354be6afea3042c6c01
slimfire <personalprojectyohannes@gmail.com>
2020-03-11 20:39:21 -0700

    [FEATURE] Connect app to store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import { CountryListView } from './accuweather';
 
 render(
-    <CountryListView />,
+    <Provider store={store}>
+        <CountryListView />
+    </Provider>,
     document.getElementById('root'),
 );
 
+export * from './services';
 export * from './constants';
 export * from './interfaces';
