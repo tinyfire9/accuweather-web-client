ac36ec96668c0076dcccc0ada5119986d6119609
slimfire <personalprojectyohannes@gmail.com>
2020-03-11 20:38:34 -0700

    [TASK] Add interface on store state

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,3 +1,5 @@
+import { Country } from './accuweather/country-list';
+
 export interface HTTPErrorResponse {
     '400': string,
     '401': string,
@@ -5,3 +7,9 @@ export interface HTTPErrorResponse {
     '500': string,
     '503': string,
 }
+
+export interface StoreState {
+    locations: {
+        countries: Country[],
+    },
+};
