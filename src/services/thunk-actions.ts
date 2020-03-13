23651aa029de3bb0dfc379442b0c18d744c4590c
slimfire <personalprojectyohannes@gmail.com>
2020-03-12 21:59:31 -0700

    [TASK] change data types on fetchData thunk action

--- src/services/thunk-actions.ts
+++ src/services/thunk-actions.ts
@@ -1,20 +1,14 @@
-import { ThunkDispatch } from 'redux-thunk';
-
 import { Action, fetchDataSuccessAction, fetchDataErrorAction } from './';
-import { StoreState } from '../interfaces'
 import { makeURL } from '../constants'
 
-export let fetchDataThunkAction = (action: Action) => {
-    return (dispatch: ThunkDispatch<StoreState, Action, any>) => {
-        console.log({
-           action
-        });
+export let fetchDataThunkAction = (action: Action): any => {
+    return (dispatch: any) => {
         let uri = makeURL(action.service, action.route);
         fetch(uri)
             .then((response: Response) => {
-                if (response.status === 200) {
+                if(response.status === 200) {
                     return response.json();
-                }
+                } 
 
                 return dispatch(
                     fetchDataErrorAction(action.feature, action.service, response.status, response.statusText),
@@ -22,7 +16,7 @@ export let fetchDataThunkAction = (action: Action) => {
             })
             .then((data: any) => {
                 dispatch(
-                    fetchDataSuccessAction(action.feature, action.service, 200, JSON.parse(JSON.stringify(data)))
+                    fetchDataSuccessAction(action.feature, action.service, 200, JSON.parse(JSON.stringify(data)), "OK")
                 );
             })
             .catch((err: Error) => {
