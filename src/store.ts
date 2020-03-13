12b9e4659f4cb78cb24b031ba7fdab288b379eb7
slimfire <personalprojectyohannes@gmail.com>
2020-03-12 21:57:27 -0700

    [TASK] add reducer on store

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { reducer } from './services/reducers'
 
-export let store = createStore(() => ({}), applyMiddleware(thunk));
+export let store = createStore(reducer, applyMiddleware(thunk));
