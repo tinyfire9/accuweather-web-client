ae9e00fbf97bdbbdda7e5d4e0a0346ead2aa7375
slimfire <personalprojectyohannes@gmail.com>
2020-03-15 15:16:59 -0400

    [FIX] return status and statusText error action object

--- src/services/thunk-actions.ts
+++ src/services/thunk-actions.ts
@@ -5,24 +5,27 @@ import { ThunkDispatch } from 'redux-thunk';
 export let fetchDataThunkAction = (action: Action): any => {
     return (dispatch: ThunkDispatch<StoreState, any, Action>) => {
         let url = makeURL(action.service, action.route);
+        let status: number, statusText: string;
         fetch(url)
             .then((response: Response) => {
-                if(response.status === 200) {
+                status = response.status;
+                statusText = response.statusText;
+                if(status === 200) {
                     return response.json();
                 } 
 
                 return dispatch(
-                    fetchDataErrorAction(action.feature, action.service, response.status, response.statusText),
+                    fetchDataErrorAction(action.feature, action.service, status, statusText),
                 );
             })
             .then((data: any) => {
                 dispatch(
-                    fetchDataSuccessAction(action.feature, action.service, 200, JSON.parse(JSON.stringify(data)), "OK")
+                    fetchDataSuccessAction(action.feature, action.service, status, JSON.parse(JSON.stringify(data)), statusText)
                 );
             })
             .catch((err: Error) => {
                 console.log({ err });
-                dispatch(fetchDataErrorAction(action.feature, action.service, -1));
+                dispatch(fetchDataErrorAction(action.feature, action.service, status, statusText));
             });
     }
 }
