d6f61ed45f13b216085519cf9f1ef0c44d0a1032
slimfire <yohazel2@gmail.com>
2020-03-19 21:25:34 -0400

    [FEATURE] Add style on layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,31 +1,31 @@
 import React, { ReactElement } from 'react';
 import { ConnectedComponent } from 'react-redux';
-import { Paper } from '@material-ui/core';
+import { Paper, Grid, withStyles } from '@material-ui/core';
+import './layout.scss';
 
 interface LayoutProps {
     navBar: ConnectedComponent<any,any> | React.Component | ReactElement | Element | null;
     views: ConnectedComponent<any,any> | React.Component | ReactElement | Element | null;
+    classes: any;
 }
 
 class Layout extends React.Component<LayoutProps, any>{
     public render() {
         return (
-            <Paper className="aw">
-                <Paper className="aw-title">
-                    AccuWeather API Dashboard
-                </Paper>
-
-                <Paper className="aw-nav-bar">
-                    { this.props.navBar }
-                </Paper>
-
-                <Paper className="aw-app">
-                    { this.props.views }
-                </Paper>
-            </Paper>
+            <Grid container spacing={3} className={this.props.classes.layout}>
+                <Grid item xs={12}><Paper className="aw-title">AccuWeather API Dashboard</Paper></Grid>
+                <Grid item sm={2}>{this.props.navBar}</Grid>
+                <Grid item xs={10} >{this.props.views}</Grid>
+            </Grid>
         );
     }
 
 }
 
-export default Layout;
+let style = {
+    layout: {
+        height: `${window.outerHeight*.80}px`
+    }
+}
+
+export default withStyles(style)(Layout);
