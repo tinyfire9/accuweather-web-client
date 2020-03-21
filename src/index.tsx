033b674e95f786d0039da2c04763df7bc3d5edba
slimfire <yohazel2@gmail.com>
2020-03-21 14:35:19 -0400

    [FEATURE] add router on app

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { Countries } from './accuweather';
+import { AccuWeather } from './accuweather';
 import { Layout, NavBar } from './components';
 
 
 render(
     <Provider store={store}>
-        <Layout navBar={<NavBar />} views={<Countries />} />
+        <BrowserRouter>
+            <Layout navBar={<NavBar />} views={<AccuWeather />} />
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root'),
 );
